9b628ce8581bacd0cf52228fd5d78db3afdb277a
PoleEast <a1525123@gmail.com>
2023-01-13 17:47:18 +0800

    Finish 1.TwoSum

---
+++ Two sum/Two_sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Two_sum
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] ans= new int[2];
+            foreach (int Iteam in nums)
+            {
+                var find = from data in nums
+                           where data == target - Iteam
+                           select data;
+                foreach(var item in find)
+                {
+                    ans = new int[] { Iteam, item };
+                }
+            }
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {2, 7, 11, 15};
+            int target = 9;
+            int[] ans = TwoSum(nums, target);
+            Console.WriteLine(ans[1].ToString()+"  "+ans[0].ToString());
+        }
+    }
+}
